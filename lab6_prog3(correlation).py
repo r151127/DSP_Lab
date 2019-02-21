a8ceff25153b8bb201f7c0bce636446ab0b4c711
r151127 <47073016+r151127@users.noreply.github.com>
2019-02-21 18:54:50 +0800

    Add files via upload

---
+++ lab6_prog3(correlation).py
@@ -0,0 +1,32 @@
+# implementation of correlation of given two signals
+import numpy as np
+import matplotlib.pyplot as plt
+a=[5,2,3]
+b=[2,4,1]
+def convolution(a,b):
+      m=np.size(a)
+      n=np.size(b)
+      c=np.zeros(m+n-1)
+      for i in range(m):
+	     for j in range(n):
+		c[i+j]=c[i+j]+a[i]*b[j]
+      return c
+
+x=list()
+p=input("enter length of x=")
+for i in range(p):
+	m=input("elements=")
+	x.append(int(m))
+print("array",x)
+y=[]
+for j in range(p):
+	y.append(m-j)
+
+t=convolution(a,b)
+print("correlation of two signals is",t) 
+plt.plot(t)
+plt.show()
+
+
+
+
